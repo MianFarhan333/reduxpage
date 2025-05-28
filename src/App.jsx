6025b6a4d37b20bc7c8b09782ac08ac049d55daf
MianFarhan333 <mianfarhan4174@gmail.com>
2025-05-28 12:44:13 +0500

    fix: correct file/folder names and imports for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Loginpage from './Pages/Loginpages'
-import Signuppage from './Pages/Signuppages'
-import Profilepage from './Pages/Profilepages'
+
+import LoginPage from './Pages/LoginPage'
+import SignupPage from './Pages/SignupPage'
+import ProfilePage from './Pages/ProfilePage'
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Loginpage />} />
-          <Route path="/signup" element={<Signuppage />} />
-          <Route path='/Profile' element={<Profilepage/>}/>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
   )
 }
 
